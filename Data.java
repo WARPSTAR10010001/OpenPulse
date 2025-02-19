defc5f52a865016eefd6dbce4cf55bcce6dcfd32
WARPSTAR ★ <alpay.gueruez@hsrw.org>
2025-02-19 12:56:37 +0100

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,29 +1,37 @@
 //Boo!!
 
 public class Data {
-    private static final String[] startSelection = new String[3];
-    private static final String[] menuSelection = new String[5];
+    private static final String[] START = new String[3];
+    private static final String[] MENU = new String[5];
+    private static final String[] SETTINGS = new String[2];
     private static boolean marginEdited = false;
     private static double margin = 1.;
 
     public static void init(){
-        startSelection[0] = "Start";
-        startSelection[1] = "Settings";
-        startSelection[2] = "Exit";
+        START[0] = "Main Menu";
+        START[1] = "Settings";
+        START[2] = "Exit";
 
-        menuSelection[0] = "Average Projection";
-        menuSelection[1] = "Monthly Savingsplan";
-        menuSelection[2] = "Owned Shares";
-        menuSelection[3] = "Back to Start";
-        menuSelection[4] = "Exit";
+        MENU[0] = "Average Projection";
+        MENU[1] = "Monthly Savingsplan";
+        MENU[2] = "Owned Shares";
+        MENU[3] = "";
+        MENU[4] = "Back to Start";
+
+        SETTINGS[0] = "Edit Margin";
+        SETTINGS[1] = "Back to Start";
     }
 
-    public static String[] getStartSelection(){
-        return startSelection;
+    public static String[] getSTART(){
+        return START;
     }
 
-    private static String[] getMenuSelection(){
-        return menuSelection;
+    public static String[] getMENU(){
+        return MENU;
+    }
+
+    public static String[] getSETTINGS(){
+        return SETTINGS;
     }
 
     public static boolean getMarginEdited(){
