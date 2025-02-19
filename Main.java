2fc5bac24c0d62d0006d75496fd38bb4090a5443
WARPSTAR ★ <alpay.gueruez@hsrw.org>
2025-02-19 08:59:58 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,27 +5,73 @@ import java.util.Scanner;
 public class Main {
     static Scanner scanner = new Scanner(System.in);
     static final String NAME = "OpenPulse";
-    static final String BUILD = "0001";
+    static final String BUILD = "0003";
     static final String DIVIDER = "\n--------------------------------------------\n";
+    static final int LENGTH = 44;
+    static final char CHARACTER = '+';
 
     public static void main(String[] args) {
         System.out.print("+++++++++++++ " + NAME + " [" + BUILD + "]" + " +++++++++++++");
-        System.out.println(DIVIDER);
+        System.out.print(DIVIDER);
         start();
     }
 
     public static void start(){
+        headline("Start");
+
         System.out.print("Edit settings? [1/0]: ");
-        switch (scanner.nextInt()){
+        int selection = scanner.nextInt();
+        switch (selection){
             case 0 -> {}
             case 1 -> settings();
+            default -> exit(true, 1);
+        }
+        scanner.nextLine();
+    }
+
+    public static void settings(){
+        headline("Settings");
+
+        System.out.print("Edit Margin? [0/1]: ");
+        int selection = scanner.nextInt();
+        switch (selection){
+            case 0 -> {}
+            case 1 -> {
+                System.out.print("  Enter margin [Current: ±" + Settings.getMargin() + "%]: ");
+                double margin = scanner.nextDouble();
+                scanner.nextLine();
+                Settings.setMargin(margin);
+                Settings.setMarginEdited(true);
+            }
             default -> exit(true, 1);
         }
         //...
     }
 
-    public static void settings(){
-        //...
+    public static String centered(String item, char sym) {
+        if(LENGTH - 4 < item.length()){
+            exit(true, 0);
+        }
+        int totalPadding = LENGTH - item.length() - 2;
+    
+        if (totalPadding <= 0) {
+            return " " + item + " ";
+        }
+        int firstHalf = totalPadding / 2;
+        int secondHalf = totalPadding - firstHalf;
+        String output = "";
+        for (int i = 0; i < firstHalf; i++) {
+            output += sym;
+        }
+        output += " " + item + " ";
+        for (int i = 0; i < secondHalf; i++) {
+            output += sym;
+        }
+        return output;
+    }
+
+    public static void headline(String item){
+        System.out.println("\n" + centered(item, CHARACTER));
     }
 
     public static void exit(boolean isError, int exitCode){
